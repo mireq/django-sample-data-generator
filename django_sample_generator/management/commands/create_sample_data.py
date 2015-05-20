0147b44e843bd34d4aa01fe697d1b49745b725fa
Miroslav Bendík <miroslav.bendik@gmail.com>
2015-05-20 13:29:53 +0200

    Django 1.9 compatibility

--- django_sample_generator/management/commands/create_sample_data.py
+++ django_sample_generator/management/commands/create_sample_data.py
@@ -4,12 +4,12 @@ import os
 import urllib
 from django.core.management.base import BaseCommand
 from django.conf import settings
-from django.utils.module_loading import import_by_path
+from django.utils.module_loading import import_string
 
 
 class Command(BaseCommand):
 	def handle(self, *args, **kwargs):
 		generators = getattr(settings, 'SAMPLE_DATA_GENERATORS', ())
 		for generator in generators:
-			generator_object = import_by_path(generator)
+			generator_object = import_string(generator)
 			generator_object.generate()
