7016776c292c956d918541a3894dbcdce2617390
Miroslav Bendík <miroslav.bendik@gmail.com>
2016-09-23 14:03:38 +0200

    Removed GeneratorRegister from public api

--- django_sample_generator/generator.py
+++ django_sample_generator/generator.py
@@ -4,35 +4,6 @@ import inspect
 from .samples import Sample
 
 
-class GeneratorRegister(object):
-	def __init__(self):
-		self.generators = []
-		self.bulk_size = 1000
-
-	def register(self, obj):
-		self.generators.append(obj)
-
-	def generate(self, command=None):
-		bulk = []
-		for generator in self.generators:
-			generator.command = command
-			if command is not None and command.verbosity > 1:
-				command.stdout.write('\nGenerating ' + generator.__class__.__name__ + ' ', ending='')
-				command.stdout.flush()
-			for obj in generator:
-				if command is not None and command.verbosity > 1:
-					command.stdout.write('.', ending='')
-					command.stdout.flush()
-				bulk.append(obj)
-				if len(bulk) >= self.bulk_size:
-					bulk[0].__class__.objects.bulk_create(bulk)
-					bulk = []
-			if bulk:
-				bulk[0].__class__.objects.bulk_create(bulk)
-				bulk = []
-			generator.done()
-
-
 class ModelGenerator(object):
 	command = None
 
@@ -53,14 +24,6 @@ class ModelGenerator(object):
 		for name, member in members:
 			setattr(obj, name, member.get_sample())
 		return obj
-
-	def next(self):
-		if self.num < self.get_count():
-			obj = self.get_object()
-			self.num += 1
-			return obj
-		else:
-			raise StopIteration()
 
 	def done(self):
 		pass
--- django_sample_generator/management/commands/create_sample_data.py
+++ django_sample_generator/management/commands/create_sample_data.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-import os
+from __future__ import unicode_literals
 
-import urllib
-from django.core.management.base import BaseCommand
 from django.conf import settings
+from django.core.management.base import BaseCommand
 from django.utils.module_loading import import_string
+
+from ...register import GeneratorRegister
 
 
 class Command(BaseCommand):
@@ -13,7 +14,10 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		self.verbosity = options.get('verbosity')
 
+		register = GeneratorRegister()
 		generators = getattr(settings, 'SAMPLE_DATA_GENERATORS', ())
-		for generator in generators:
-			generator_object = import_string(generator)
-			generator_object.generate(command=self)
+		for generator_path in generators:
+			generator_module = import_string(generator_path + '.generators')
+			for generator in generator_module:
+				register.register(generator)
+		register.generate(command=self)
