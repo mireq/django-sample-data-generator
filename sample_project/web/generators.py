c710e02966efe126a8f811d6949f7a53b418cce3
Miroslav Bendík <miroslav.bendik@gmail.com>
2016-09-24 19:54:34 +0200

    Updated sample project

--- sample_project/web/generators.py
+++ sample_project/web/generators.py
@@ -1,16 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django_sample_generator import generator
+from django_sample_generator import generator, fields, functions
 
-from .models import Category
+from .models import Category, Article
 
 
 class CategoryGenerator(generator.ModelGenerator):
 	class Meta:
 		model = Category
+		field_kwargs = { # TODO: implement
+			'slug': {'max_length': 2},
+		}
+
+
+class ArticleGenerator(generator.ModelGenerator):
+	content = fields.function_field_generator(functions.gen_text_long)
+
+	class Meta:
+		model = Article
+		exclude = ('slug',) # TODO: implement
 
 
 generators = [
 	CategoryGenerator(10),
+	ArticleGenerator(100),
 ]
--- sample_project/web/models.py
+++ sample_project/web/models.py
@@ -7,3 +7,11 @@ from django.db import models
 class Category(models.Model):
 	name = models.CharField(max_length=100)
 	slug = models.SlugField(max_length=20, unique=True)
+
+
+class Article(models.Model):
+	category = models.ForeignKey(Category)
+	name = models.CharField(max_length=100)
+	slug = models.SlugField(max_length=20, unique=True)
+	perex = models.TextField()
+	content = models.TextField()
