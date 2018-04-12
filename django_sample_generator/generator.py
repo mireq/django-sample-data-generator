2d68e68b28468f2513c877e9d5bea440ca6f2f33
Miroslav Bendík <miroslav.bendik@gmail.com>
2018-04-12 16:55:55 +0200

    Better error message

--- django_sample_generator/generator.py
+++ django_sample_generator/generator.py
@@ -33,7 +33,7 @@ def field_to_generator(field, opts):
 	if field_cls in GENERATORS:
 		return GENERATORS[field_cls](**opts.field_kwargs.get(field.name, {}))
 	else:
-		raise RuntimeError("Field %s is not registered" % str(field_cls))
+		raise RuntimeError("Field %s (%s) is not registered" % (field.name, str(field_cls)))
 
 
 class ModelGeneratorBase(type):
