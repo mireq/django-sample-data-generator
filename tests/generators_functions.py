ee4cd410c35b4da5f74095b97c0bff56b27fea19
Miroslav Bendík <miroslav.bendik@gmail.com>
2022-12-31 15:10:36 +0100

    test: Removed metaclasses, define model using constructor

--- tests/generators_functions.py
+++ tests/generators_functions.py
@@ -6,29 +6,20 @@ from django_sample_generator import generator, fields
 class BlankTextGenerator(generator.ModelGenerator):
 	field = fields.CharFieldGenerator(blank=True, max_length=0)
 
-	class Meta:
-		model = Foo
-
 
 class BlankSlugGenerator(generator.ModelGenerator):
 	field = fields.SlugFieldGenerator(blank=True, max_length=0)
-
-	class Meta:
-		model = Foo
 
 
 class SeqIntegerGenerator(generator.ModelGenerator):
 	field = fields.SeqIntegerFieldGenerator(start=10, step=5)
 
-	class Meta:
-		model = Foo
-
 
 generators = []
 test_generators = {
-	'blank': [BlankTextGenerator(1)],
-	'blank_slug': [BlankSlugGenerator(1)],
-	'seq': [SeqIntegerGenerator(2)],
+	'blank': [BlankTextGenerator(1, model=Foo)],
+	'blank_slug': [BlankSlugGenerator(1, model=Foo)],
+	'seq': [SeqIntegerGenerator(2, model=Foo)],
 }
 
 
